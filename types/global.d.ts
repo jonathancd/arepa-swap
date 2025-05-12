1306ebb627577978039749915b9f71b45e71e197
Jonathan Cuotto <jonathancuotto@gmail.com>
2025-05-12 11:34:56 +0400

    chore: setup base monorepo structure with TypeScript, shared tsconfig, and UI package ready

---
+++ types/global.d.ts
@@ -0,0 +1,10 @@
+interface EthereumProvider {
+  isMetaMask?: boolean;
+  request: (...args: any[]) => Promise<any>;
+  on?: (...args: any[]) => void;
+  removeListener?: (...args: any[]) => void;
+}
+
+interface Window {
+  ethereum?: EthereumProvider;
+}
